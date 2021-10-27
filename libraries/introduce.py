94d2eaddd13b31f6082aea7a1fccdb40c3777bfb
luizfelipebraga <ruivolfb@gmail.com>
2021-10-27 09:31:36 -0300

    feat(packages) add some packages

---
+++ libraries/introduce.py
@@ -0,0 +1,13 @@
+#1. Pegar o conteúdo Html a partir de URL
+#2. Parsear o conteúdo Html - BeatifulfulSoup
+#3. Estrturuar conteúdo em um Data Frame - Pandas
+#4. Transformar os Dados em um Dicionário de dados próprio
+#5. Converter e salvar em um arquivo JSON.
+
+#Libraries#
+#pip install:
+  # requests2
+  # pandas
+  # lxml
+  # beatifulsoup4
+  # selenium
